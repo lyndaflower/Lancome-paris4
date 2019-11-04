12bd5aaec0bef8e40c5476147168b6ae3977bc9e
lyndaflower <umurerwalynda@gmail.com>
2019-11-04 09:17:19 +0200

    add new activity to save our choices to firebbse

---
+++ app/src/main/java/com/example/androidproject/activities/Main2Activity.java
@@ -0,0 +1,13 @@
+package com.example.androidproject.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
