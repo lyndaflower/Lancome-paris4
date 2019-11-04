64f87e2408664f68026821efb011926816cdcc42
lyndaflower <umurerwalynda@gmail.com>
2019-11-04 09:35:38 +0200

    initialize databse in holer adapter

--- app/src/main/java/com/example/androidproject/activities/SavePerfumesActivity.java
+++ app/src/main/java/com/example/androidproject/activities/SavePerfumesActivity.java
@@ -4,7 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.androidproject.adapters.HolderAdapter;
+import com.example.androidproject.models.Perfume;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+
 public class SavePerfumesActivity extends AppCompatActivity {
+    private DatabaseReference PerfumesReference;
+    private FirebaseRecyclerAdapter<Perfume, HolderAdapter> mFirebaseAdapter;
+
+    public SavePerfumesActivity(DatabaseReference perfumesReference) {
+        PerfumesReference = perfumesReference;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
